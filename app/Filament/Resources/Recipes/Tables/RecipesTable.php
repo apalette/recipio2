b05aec608d2140c23613a5c95ba556664e0a60b4
apalette <aurelie@codeuz.com>
2026-05-02 14:24:59 +0200

    feat: Recipe Resource

--- app/Filament/Resources/Recipes/Tables/RecipesTable.php
+++ app/Filament/Resources/Recipes/Tables/RecipesTable.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\Recipes\Tables;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
 class RecipesTable
@@ -13,8 +14,37 @@ class RecipesTable
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('id')
+                    ->sortable(),
+
+                TextColumn::make('title')
+                    ->label('Titre')
+                    ->searchable()
+                    ->sortable(),
+
+                TextColumn::make('slug')
+                    ->searchable()
+                    ->copyable(),
+
+                TextColumn::make('time_min')
+                    ->label('Durée mini (min)')
+                    ->sortable(),
+
+                TextColumn::make('time_max')
+                    ->label('Durée maxi (min)')
+                    ->sortable(),
+
+                TextColumn::make('created_at')
+                    ->label('Créé le')
+                    ->dateTime('d/m/Y H:i')
+                    ->sortable(),
+
+                TextColumn::make('updated_at')
+                    ->label('Modifié le')
+                    ->dateTime('d/m/Y H:i')
+                    ->sortable(),
             ])
+            ->defaultSort('title')
             ->filters([
                 //
             ])
